0077eba116799bd13f64cea99aab8753a743d884
Livia Kuhn <liv.kuhn@gmail.com>
2020-11-25 10:15:47 +0100

    [NW] Add list of unigrams and bigrams

--- generate_wordcloud.py
+++ generate_wordcloud.py
@@ -3,6 +3,15 @@ import matplotlib.pyplot as plt
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
 
+LINKEDIN_COLORS = ['#0077B5', '#dce6f1']
+CONTEXTA_COLORS = ['#00adee', '#e4097f']
+
+BACKGROUND_COLOR = '#FFFFFF'
+FONT_PATH = '/home/livia/font/Roboto-Regular.ttf'
+COLORMAP = 'YlOrRd'
+REGEXP = r'\w+( [\w]+)?'
+
+
 def parse_args():
     parser = ap.ArgumentParser(
         description='Generate a word cloud from a list of words.')
@@ -13,12 +22,22 @@ def parse_args():
 
 def read_word_list(words_in):
     with open(words_in, 'r') as src:
-        words = src.read()
-        return words
+        words = [word.strip('\n') for word in src]
+        word_dic = {}
+        for word in words:
+            if word not in word_dic:
+                word_dic[word] = 1
+            elif word in word_dic:
+                word_dic[word] += 1
+        return word_dic
 
 
-def generate_wordcloud(words):
-    wordcloud = WordCloud().generate(words)
+def generate_wordcloud(word_dic):
+    wordcloud = WordCloud(prefer_horizontal=1,
+                          background_color=BACKGROUND_COLOR,
+                          font_path=FONT_PATH,
+                          colormap=COLORMAP,
+                          regexp=REGEXP).generate(word_dic)
     return wordcloud
 
 
@@ -34,8 +53,8 @@ def display_wordcloud(wordcloud):
 
 def main():
     args = parse_args()
-    words = read_word_list(args.words_in)
-    wordcloud = generate_wordcloud(words)
+    word_dic = read_word_list(args.words_in)
+    wordcloud = generate_wordcloud(word_dic)
     save_wordcloud(args.wordcloud_out, wordcloud)
     display_wordcloud(wordcloud)
 
